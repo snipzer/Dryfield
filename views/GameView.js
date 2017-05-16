5010d103ddce9df459dd5672d92e96e4ecbdf114
snipzer <l.arif@laposte.net>
2017-05-16 17:02:33 +0200

    Début de la création de la vue et génération automatique du html pour les fields

--- views/GameView.js
+++ views/GameView.js
@@ -1,3 +1,45 @@
-/**
- * Created by snipzer on 16/05/17.
- */
+function GameView()
+{
+    EventEmitter.call(this);
+    this.init();
+}
+
+GameView.prototype = Object.create(EventEmitter.prototype);
+GameView.prototype.constructor = GameView;
+
+GameView.prototype.init = function ()
+{
+    document.querySelector("#field1-irriguer").onsubmit = (function (e)
+    {
+        e.preventDefault();
+
+        this.emit("change-username", {
+            username: document.getElementById('username').value
+        });
+    }).bind(this);
+
+    this.on("update-score", function (data)
+    {
+        document.querySelector('#recolte').innerText = data.score;
+    });
+
+    this.on("update-global-water", function (data)
+    {
+        document.querySelector("#water").innerText = data.waterLevel;
+    });
+
+    this.on("update-money", function (data)
+    {
+        document.querySelector('#money').innerText = data.money;
+    });
+
+    this.on("update-water", function (data)
+    {
+        document.querySelector('#'+data.id+"water_level").innerText = data.score;
+    });
+
+    this.on("update-maturation", function (data)
+    {
+        document.querySelector("#"+data.id+"maturation").innerText = data.maturation;
+    });
+};
