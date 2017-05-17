c28096866cddcb4e31f3e4ac1b135327f9c50d52
snipzer <l.arif@laposte.net>
2017-05-17 16:01:05 +0200

    ldkfjdlkfj

--- views/GameView.js
+++ views/GameView.js
@@ -75,18 +75,18 @@ GameView.prototype.init = function ()
     this.user.on("update-score", function (data)
     {
         document.querySelector('#rec').innerText = data.score;
-        //document.querySelector('#rec-modal').innerText = data.score;
+        document.querySelector('#rec-modal').innerText = data.score;
     });
 
     this.user.on("update-global-water", function (data)
     {
         document.querySelector("#lit").innerText = data.waterLevel;
-        //document.querySelector("#lit-modal").innerText = data.waterLevel;
+        document.querySelector("#lit-modal").innerText = data.waterLevel;
     });
 
     this.user.on("update-money", function (data)
     {
         document.querySelector('#arg').innerText = data.money;
-        //document.querySelector('#arg-modal').innerText = data.money;
+        document.querySelector('#arg-modal').innerText = data.money;
     });
 };
